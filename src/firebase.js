e354bbafae1c6ef173bdd968f85bcf3bc7bb5607
Rakhi Gupta <guptarudra852@gmail.com.com>
2025-12-25 13:55:02 +0530

    Fixed Tailwind and React structure

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB10bjjTfKcJPdSpNNXm0Qmsz2UKjkM3zc",
-  authDomain: "nexttoppers-pro.firebaseapp.comP",
+  authDomain: "nexttoppers-pro.firebaseapp.com",
   databaseURL: "https://nexttoppers-pro-default-rtdb.firebaseio.com",
   projectId: "nexttoppers-pro",
-  storageBucket: "nexttoppers-pro.firebasestorage.app",
+  storageBucket: "nexttoppers-pro.appspot.com",
   messagingSenderId: "515147458590",
   appId: "1:515147458590:web:91df42397801df433be60f",
 };
