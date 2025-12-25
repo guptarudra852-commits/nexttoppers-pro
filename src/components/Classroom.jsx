d3e30f68fba40e422db3690408abc08dcdf86bc9
Rakhi Gupta <guptarudra852@gmail.com.com>
2025-12-25 13:27:15 +0530

    Fixed Tailwind and React structure

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,4 +1,8 @@
-function App() {
-  return <Classroom />
+export default function Classroom() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Live Classroom</h1>
+      <p>Class will start soon...</p>
+    </div>
+  );
 }
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.html' // Just to be sure styles load
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // ✅ CSS only
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
